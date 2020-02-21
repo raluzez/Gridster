c890fe836a9b8f6ae7522e3827d6b0696aa90bbe
Lukas Laurinavicius <lukaslaurina@gmail.com>
2020-02-21 15:48:13 +0200

    config

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+    color: red;
+`;
+
+const App = () => {
+    return (
+        <div>
+            <Title>Labas!</Title>
+        </div>
+    );
+}
+
+export default App;
